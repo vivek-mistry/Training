8a7ef70f63ad48e2df950a5472e1e0c40e077554
vivek-mistry <vickymistry1818@gmail.com>
2025-11-04 11:22:53 +0530

    - SubCategory controller, model and migration ✅

---
+++ app/Models/SubCategory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SubCategory extends Model
+{
+    protected $table = "sub_categories";
+
+    protected $fillable = [
+        "category_id",
+        "name"
+    ];
+}
