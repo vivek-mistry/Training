226b2f3bf256deb85557c7924bbb6272ee63d21b
vivek-mistry <vickymistry1818@gmail.com>
2025-11-04 11:18:14 +0530

    - Category Controller, model and migration ✅

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -7,5 +7,12 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    // Listing Load View
+    public function index()
+    {
+        // Data Fetch
+        // Pass To View
+        // Load View with Data
+
+    }
 }
