d581313741088a46d2572243d75d22e11b7123af
vivek-mistry <vickymistry1818@gmail.com>
2025-11-19 11:19:22 +0530

    - register UI implemented ✅

--- resources/views/backend/register.blade.php
+++ resources/views/backend/register.blade.php
@@ -1,24 +1,118 @@
-<html>
-    <head>
-        <title>Register</title>
-    </head>
-    <body>
-        <h1>Register</h1>
-        <form action="{{ route('register_store_user') }}" method="POST">
-            @csrf
-            <div>
-                <label for="name">Name:</label>
-                <input type="text" id="name" name="name" required>
+<!doctype html>
+<html lang="en" data-bs-theme="blue-theme">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Maxton | Bootstrap 5 Admin Dashboard Template</title>
+    <!--favicon-->
+    <link rel="icon" href="{{ asset('backend/assets/images/favicon-32x32.png') }}" type="image/png">
+    <!-- loader-->
+    <link href="{{ asset('backend/assets/css/pace.min.css') }}" rel="stylesheet">
+    <script src="{{ asset('backend/assets/js/pace.min.js') }}"></script>
+
+    <!--plugins-->
+    <link href="{{ asset('backend/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{ asset('backend/assets/plugins/metismenu/metisMenu.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('backend/assets/plugins/metismenu/mm-vertical.css') }}">
+    <!--bootstrap css-->
+    <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/assets/css/bootstrap-extended.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Material+Icons+Outlined" rel="stylesheet">
+    <!--main css-->
+    <link href="{{ asset('backend/sass/main.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/sass/dark-theme.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/sass/blue-theme.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/sass/responsive.css') }}" rel="stylesheet">
+
+</head>
+
+<body class="bg-register">
+
+
+    <!--authentication-->
+
+    <div class="container-fluid my-5">
+        <div class="row">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5 col-xxl-5 mx-auto">
+                <div class="card rounded-4 mb-0 border-top border-4 border-primary border-gradient-1">
+                    <div class="card-body p-5">
+                        <img src="assets/images/logo1.png" class="mb-4" width="145" alt="">
+                        <h4 class="fw-bold">Get Started Now</h4>
+                        <p class="mb-0">Enter your credentials to create your account</p>
+
+                        <div class="form-body my-4">
+                            <form class="row g-3" method="POST" action="{{ route('register_store_user') }}">
+                                @csrf
+                                <div class="col-12">
+                                    <label for="inputUsername" class="form-label">Name</label>
+                                    <input type="text" class="form-control" id="inputUsername" name="name" placeholder="Jhon" required>
+                                </div>
+                                <div class="col-12">
+                                    <label for="inputEmailAddress" class="form-label">Email Address</label>
+                                    <input type="email" class="form-control" name="email" id="inputEmailAddress"
+                                        placeholder="example@user.com">
+                                </div>
+                                <div class="col-12">
+                                    <label for="inputChoosePassword" class="form-label">Password</label>
+                                    <div class="input-group" id="show_hide_password">
+                                        <input type="password" name="password" class="form-control border-end-0"
+                                            id="inputChoosePassword" value="12345678" placeholder="Enter Password">
+                                        <a href="javascript:;" class="input-group-text bg-transparent"><i
+                                                class="bi bi-eye-slash-fill"></i></a>
+                                    </div>
+                                </div>
+                                
+                                
+                                
+                                <div class="col-12">
+                                    <div class="d-grid">
+                                        <button type="submit" class="btn btn-grd-danger">Register</button>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="text-start">
+                                        <p class="mb-0">Already have an account? <a href="{{ route('login') }}">Sign
+                                                in here</a></p>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+
+                        
+
+                    </div>
+                </div>
             </div>
-            <div>
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required>
-            </div>
-            <div>
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
-            </div>
-            <button type="submit">Register</button>
-        </form>
-    </body>
+        </div><!--end row-->
+    </div>
+
+    <!--authentication-->
+
+
+
+
+    <!--plugins-->
+    <script src="{{ asset('backend/assets/js/jquery.min.js') }}"></script>
+
+    <script>
+        $(document).ready(function() {
+            $("#show_hide_password a").on('click', function(event) {
+                event.preventDefault();
+                if ($('#show_hide_password input').attr("type") == "text") {
+                    $('#show_hide_password input').attr('type', 'password');
+                    $('#show_hide_password i').addClass("bi-eye-slash-fill");
+                    $('#show_hide_password i').removeClass("bi-eye-fill");
+                } else if ($('#show_hide_password input').attr("type") == "password") {
+                    $('#show_hide_password input').attr('type', 'text');
+                    $('#show_hide_password i').removeClass("bi-eye-slash-fill");
+                    $('#show_hide_password i').addClass("bi-eye-fill");
+                }
+            });
+        });
+    </script>
+
+</body>
+
 </html>
