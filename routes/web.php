e51cdff345e6d8bdae6285f05c2b1bced59bd66e
vivek-mistry <vickymistry1818@gmail.com>
2025-11-19 11:10:12 +0530

    - Restrict Inner pages with Middleware ✅

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Backend\AuthenticateController;
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\ProductController;
 use App\Http\Controllers\Backend\SubCategoryController;
+use App\Http\Middleware\BackendMiddleware;
 use Illuminate\Support\Facades\Route;
 
 
@@ -23,25 +24,25 @@ Route::get('email', function () {
     return view('backend.email');
 });
 
-Route::get('categories', [CategoryController::class, 'index'])->name('category_list');
-Route::get('categories/create', [CategoryController::class, 'create'])->name('category_create');
-Route::post('categories/store', [CategoryController::class, 'store'])->name('category_store');
-Route::get('categories/remove/{id}', [CategoryController::class,'delete'])->name('category_delete');
-Route::get('categories/edit/{id}', [CategoryController::class, 'edit'])->name('category_edit');
-Route::post('categories/update/{id}', [CategoryController::class, 'update'])->name('category_update');
+Route::get('categories', [CategoryController::class, 'index'])->name('category_list')->middleware([BackendMiddleware::class]);
+Route::get('categories/create', [CategoryController::class, 'create'])->name('category_create')->middleware([BackendMiddleware::class]);
+Route::post('categories/store', [CategoryController::class, 'store'])->name('category_store')->middleware([BackendMiddleware::class]);
+Route::get('categories/remove/{id}', [CategoryController::class,'delete'])->name('category_delete')->middleware([BackendMiddleware::class]);
+Route::get('categories/edit/{id}', [CategoryController::class, 'edit'])->name('category_edit')->middleware([BackendMiddleware::class]);
+Route::post('categories/update/{id}', [CategoryController::class, 'update'])->name('category_update')->middleware([BackendMiddleware::class]);
 
-Route::get('sub_categories', [SubCategoryController::class, 'index'])->name('sub_category_list');
-Route::get('sub_categories/create', [SubCategoryController::class, 'create'])->name('sub_category_create');
-Route::post('sub_categories/store', [SubCategoryController::class, 'store'])->name('sub_category_store');
-Route::get('sub_categories/edit/{id}', [SubCategoryController::class, 'edit'])->name('sub_category_edit');
-Route::post('sub_categories/update/{id}', [SubCategoryController::class, 'update'])->name('sub_category_update');
-Route::get('sub_categories/fetchDropDownSubCategory/{category_id}', [SubCategoryController::class, 'fetchDropDownSubCategory'])->name('sub_category_fetchDropDownSubCategory');
+Route::get('sub_categories', [SubCategoryController::class, 'index'])->name('sub_category_list')->middleware([BackendMiddleware::class]);
+Route::get('sub_categories/create', [SubCategoryController::class, 'create'])->name('sub_category_create')->middleware([BackendMiddleware::class]);
+Route::post('sub_categories/store', [SubCategoryController::class, 'store'])->name('sub_category_store')->middleware([BackendMiddleware::class]);
+Route::get('sub_categories/edit/{id}', [SubCategoryController::class, 'edit'])->name('sub_category_edit')->middleware([BackendMiddleware::class]);
+Route::post('sub_categories/update/{id}', [SubCategoryController::class, 'update'])->name('sub_category_update')->middleware([BackendMiddleware::class]);
+Route::get('sub_categories/fetchDropDownSubCategory/{category_id}', [SubCategoryController::class, 'fetchDropDownSubCategory'])->name('sub_category_fetchDropDownSubCategory')->middleware([BackendMiddleware::class]);
 
-Route::get('products', [ProductController::class, 'index'])->name('product_list');
-Route::get('products/create', [ProductController::class, 'create'])->name('product_create');
-Route::post('products/store', [ProductController::class, 'store'])->name('product_store');
-Route::get('products/edit/{product}', [ProductController::class, 'edit'])->name('product_edit');
-Route::post('products/update/{id}', [ProductController::class, 'update'])->name('product_update');
+Route::get('products', [ProductController::class, 'index'])->name('product_list')->middleware([BackendMiddleware::class]);
+Route::get('products/create', [ProductController::class, 'create'])->name('product_create')->middleware([BackendMiddleware::class]);
+Route::post('products/store', [ProductController::class, 'store'])->name('product_store')->middleware([BackendMiddleware::class]);
+Route::get('products/edit/{product}', [ProductController::class, 'edit'])->name('product_edit')->middleware([BackendMiddleware::class]);
+Route::post('products/update/{id}', [ProductController::class, 'update'])->name('product_update')->middleware([BackendMiddleware::class]);
 
 Route::get('login', [AuthenticateController::class, 'singIn'])->name('login');
 Route::post('authenticate', [AuthenticateController::class, 'authetnicateCheck'])->name('authenticate');
